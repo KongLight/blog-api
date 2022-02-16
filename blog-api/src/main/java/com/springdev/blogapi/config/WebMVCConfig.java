00805808c3f762402f1dac378b29056bf7cba40f
Chen <562147487@qq.com>
2022-02-16 22:03:59 +0800

    登录拦截器

--- blog-api/src/main/java/com/springdev/blogapi/config/WebMVCConfig.java
+++ blog-api/src/main/java/com/springdev/blogapi/config/WebMVCConfig.java
@@ -1,7 +1,10 @@
 package com.springdev.blogapi.config;
 
+import com.springdev.blogapi.handler.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,9 +14,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //跨域配置
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截test接口，后续实际遇到需要拦截的接口时，配置为真正的拦截接口
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/test");
     }
 }
